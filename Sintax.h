fa5331d50644881fc411fb29d68c4300240501d3
AnnaGorina <49533436+AnnaGorina@users.noreply.github.com>
2019-11-28 09:02:16 +0300

    Create Sintax.h

---
+++ Sintax.h
@@ -0,0 +1,41 @@
+#ifndef SINTAX_H
+#define SINTAX_H
+#include <string>
+#include <iostream>
+#include "lexem.h"
+#include <vector>
+
+class Lexem;
+
+class Sintax
+{
+    //Sintax();
+
+    lex Lexems;
+    int pozision = 0;
+
+    std::vector<lex>* Lex;
+
+
+
+public:
+    Sintax();
+
+    void S(std::vector<lex>* lexems);
+    void A();
+    void B();
+    void C();
+    void D();
+    void E();
+    void F();
+    void G();
+    void H();
+    void I();
+    void J();
+
+    bool Proverka();
+
+    ~Sintax();
+};
+
+#endif // SINTAX_H
