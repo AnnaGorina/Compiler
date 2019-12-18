e3a05f6713710306862f4d510b7929a191ad9746
AnnaGorina <49533436+AnnaGorina@users.noreply.github.com>
2019-12-18 22:26:46 +0300

    Create GenerateCode.h

---
+++ GenerateCode.h
@@ -0,0 +1,27 @@
+#ifndef GENERATECODE_H
+#define GENERATECODE_H
+#include <lexem.h>
+#include <sintax.h>
+#include <vector>
+#include <fstream>
+#include <clocale>
+
+class Lexem;
+class Sintax;
+
+class GenerateCode
+{
+    lex Lexems;
+    int pozition = 0;
+
+    std::vector<lex>* Lex;
+
+public:
+    GenerateCode();
+
+    void Generation(std::vector<lex> *lexems);
+
+    ~GenerateCode();
+};
+
+#endif // GENERATECODE_H
